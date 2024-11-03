fae26db65d2cfc7e46dbc7b534217c3f8910ed00
Alexandr Ryabinin <zochsa@gmail.com>
2024-11-03 18:21:34 +0300

    Add NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,21 +1,51 @@
 package hexlet.code.schemas;
 
 public class NumberSchema {
+    private boolean state = true;
+
+    private boolean flagRequired = false;
+    private boolean flagPositive = false;
+    private boolean flagRange = false;
+    private Integer left;
+    private Integer right;
 
     // Проверка на null
     public NumberSchema required() {
+        flagRequired = true;
         return this;
     }
 
     // Проверка на положетельное число
     public NumberSchema positive() {
+        flagPositive = true;
         return this;
     }
 
     public NumberSchema range(Integer left, Integer right){
+        flagRange = true;
+        this.left = left;
+        this.right = right;
         return this;
     }
     public boolean isValid(Integer number) {
-        return true;
+        if (flagRequired) {
+            if (state == true) {
+                state = number!= null;
+            }
+        }
+
+        if (flagPositive) {
+            if (state == true) {
+                state = number > 0;
+            }
+        }
+
+        if (flagRange) {
+            if (state == true) {
+                state = number >= left && number <= right;
+            }
+        }
+
+        return state;
     }
 }
