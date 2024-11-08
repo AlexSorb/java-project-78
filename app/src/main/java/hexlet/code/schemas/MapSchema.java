171c11c722f233d0cc847b670b924850f7609899
Alexandr Ryabinin <zochsa@gmail.com>
2024-11-08 22:52:38 +0300

    Add tests for shape function

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,8 +3,10 @@ package hexlet.code.schemas;
 import java.util.Map;
 
 public class MapSchema extends BaseSchema<Map<? extends Object, ? extends Object>> {
-    boolean sizeOfFlag = false;
-    int checkedSize = 0;
+    private boolean sizeOfFlag = false;
+    private int checkedSize = 0;
+    private boolean shapeFlag = false;
+
     public MapSchema sizeof(int size) {
         sizeOfFlag = true;
         checkedSize = size;
@@ -19,5 +21,9 @@ public class MapSchema extends BaseSchema<Map<? extends Object, ? extends Object
             isValid = mapSize == checkedSize;
         }
         return isValid;
+    }
+
+    public void shape(Map<? extends Object, BaseSchema<String>> schema) {
+        shapeFlag = true;
     }
 }
--- app/src/test/java/hexlet/code/schemas/ShapeFunctionTest.java
+++ app/src/test/java/hexlet/code/schemas/ShapeFunctionTest.java
@@ -1,4 +1,43 @@
 package hexlet.code.schemas;
 
+import hexlet.code.Validator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShapeFunctionTest {
+    private Validator validator = new Validator();
+    private MapSchema schema;
+    private HashMap<String, BaseSchema<String>> schemas = new HashMap<>();
+
+    @BeforeEach
+    public void initialization() {
+        schema = validator.map();
+        schemas.put("firstName", validator.string().required());
+        schemas.put("lastName", validator.string().required().minLength(2));
+    }
+
+    @Test
+    public void shapeTest() {
+        schema.shape(schemas);
+
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+        assertTrue(schema.isValid(human1));
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+        assertFalse(schema.isValid(human2));
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+        assertFalse(schema.isValid(human3)); // false
+    }
 }
