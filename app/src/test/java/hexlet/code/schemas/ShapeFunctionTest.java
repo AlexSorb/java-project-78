b43912e4796d0a39d3215a51de13e846088b94ac
Alexandr Ryabinin <zochsa@gmail.com>
2025-01-28 23:30:57 +0300

    update ShapeFunctionTest

--- app/src/test/java/hexlet/code/schemas/ShapeFunctionTest.java
+++ app/src/test/java/hexlet/code/schemas/ShapeFunctionTest.java
@@ -10,34 +10,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShapeFunctionTest {
-    private Validator validator = new Validator();
+    private final static Validator VALIDATOR = new Validator();
     private MapSchema schema;
     private HashMap<String, BaseSchema> schemas = new HashMap<>();
 
     @BeforeEach
     public void initialization() {
-        schema = validator.map();
-        schemas.put("firstName", validator.string().required());
-        schemas.put("lastName", validator.string().required().minLength(2));
+        schema = VALIDATOR.map();
+        schemas.put("firstName", VALIDATOR.string().required());
+        schemas.put("lastName", VALIDATOR.string().required().minLength(2));
     }
 
     @Test
     public void shapeTest() {
         schema.shape(schemas);
 
-//        Map<String, String> human1 = new HashMap<>();
-//        human1.put("firstName", "John");
-//        human1.put("lastName", "Smith");
-//        assertTrue(schema.isValid(human1));
-//
-//        Map<String, String> human2 = new HashMap<>();
-//        human2.put("firstName", "John");
-//        human2.put("lastName", null);
-//        assertFalse(schema.isValid(human2));
-//
-//        Map<String, String> human3 = new HashMap<>();
-//        human3.put("firstName", "Anna");
-//        human3.put("lastName", "B");
-//        assertFalse(schema.isValid(human3)); // false
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+        assertTrue(schema.isValid(human1));
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+        assertFalse(schema.isValid(human2));
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+        assertFalse(schema.isValid(human3)); // false
     }
 }
